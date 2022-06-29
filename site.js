a18aa6d2aecde73a6af6f08ab64b76a2268ad727
satellitecanvas <[stephen.thorsteinsson@gmail.com]>
2022-06-29 12:32:50 -0400

    added js to make the visibility change on click but not perfect yet

--- site.js
+++ site.js
@@ -1,7 +1,21 @@
-<h3 id="demo" onclick="myFunction()">Click me to change my color.</h3>
+function aboutMeFunction() {
+  document.getElementById("aboutme").style.zIndex = "1";
+  document.getElementById("aboutme").style.visibility = "visible";
+  document.getElementById("projects").style.visibility = "hidden";
+  document.getElementById("skills").style.visibility = "hidden";
+};
 
-<script>
-function myFunction() {
-  document.getElementById("demo").style.color = "red";
-}
-</script>
+function projectsFunction() {
+  document.getElementById("projects").style.zIndex = "1";
+  document.getElementById("projects").style.visibility = "visible";
+  document.getElementById("aboutme").style.visibility = "hidden";
+  document.getElementById("skills").style.visibility = "hidden";
+}:
+
+function skillsFunction() {
+  document.getElementById("skills").style.zIndex = "1";
+  document.getElementById("skills").style.visibility = "visible";
+  document.getElementById("aboutme").style.visibility = "hidden";
+  document.getElementById("projects").style.visibility = "hidden";
+};
+
