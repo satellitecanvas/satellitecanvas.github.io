769f0ef455bbb5fbca6c03f89a4e0ef4cda0b03b
satellitecanvas <[stephen.thorsteinsson@gmail.com]>
2022-07-07 10:51:49 -0400

    more experimenting with external js still will not work

--- site.js
+++ site.js
@@ -1,21 +1,24 @@
-function aboutMeFunction() {
-  document.getElementById("aboutme").style.zIndex = "1";
-  document.getElementById("aboutme").style.visibility = "visible";
-  document.getElementById("projects").style.visibility = "hidden";
-  document.getElementById("skills").style.visibility = "hidden";
-};
+var aboutMe = (function($){
+  return {
+      aboutMe: function() {
+        document.getElementById("aboutme").style.zIndex = "1";
+        document.getElementById("aboutme").style.visibility = "visible";
+        document.getElementById("projects").style.visibility = "hidden";
+        document.getElementById("skills").style.visibility = "hidden";
+      },
+  };
+}(jQuery));
 
-function projectsFunction() {
+var projects = function() {
   document.getElementById("projects").style.zIndex = "1";
   document.getElementById("projects").style.visibility = "visible";
   document.getElementById("aboutme").style.visibility = "hidden";
   document.getElementById("skills").style.visibility = "hidden";
 }:
 
-function skillsFunction() {
+var skills = function() {
   document.getElementById("skills").style.zIndex = "1";
   document.getElementById("skills").style.visibility = "visible";
   document.getElementById("aboutme").style.visibility = "hidden";
   document.getElementById("projects").style.visibility = "hidden";
 };
-
